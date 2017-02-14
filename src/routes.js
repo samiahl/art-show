8967ad93b511a91981ba2c60c96329b524d4b561
Sami Åhl <sami.leo.ahl@gmail.com>
2017-02-14 20:02:50 +0200

    added routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,20 @@
 /**
  * Created by sami on 9.2.2017.
  */
+
+import React from 'react';
+import { Route, Indexroute } from 'react-router';
+import Layout from './components/Layout';
+import IndexPage from './components/IndexPage';
+import ArtistPage from './components/artistPage';
+import NotFoundPage from './components/NotFoundPage';
+
+const routes = (
+    <Route path="/" component={Layout}>
+        <IndexRoute component={IndexPage}/>
+        <Route path="artist/:id" component={ArtistPage}/>
+        <Route path="*" component={NotFoundPage}/>
+    </Route>
+);
+
+export default routes;
